fb5af0035fa6f53abcf0bdd7767fb6de49e3fc52
Alan Edwardes <alan@alanedwardes.com>
2026-04-13 23:51:46 +0100

    Update to .NET 10.

---
+++ src/Ae.Freezer.BunnyCdn/BunnyCdnWebsiteResourceWriter.cs
@@ -0,0 +1,70 @@
+using Ae.Freezer.Entities;
+using Ae.Freezer.Writers;
+using BunnyCDN.Net.Storage;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ae.Freezer.BunnyCdn
+{
+    /// <summary>
+    /// Describes a <see cref="IWebsiteResourceWriter"/> implementation which writes <see cref="WebsiteResource"/> objects to Bunny CDN using the correct paths.
+    /// </summary>
+    public sealed class BunnyCdnWebsiteResourceWriter : IWebsiteResourceWriter
+    {
+        private readonly ILogger<BunnyCdnWebsiteResourceWriter> _logger;
+        private readonly BunnyCDNStorage _bunnyCdnStorage;
+        private readonly BunnyCdnWebsiteResourceWriterConfiguration _configuration;
+
+        /// <summary>
+        /// Construct a new <see cref="BunnyCdnWebsiteResourceWriter"/> using the specified <see cref="BunnyCDNStorage"/> client and <see cref="BunnyCdnWebsiteResourceWriterConfiguration"/> object.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="configuration"></param>
+        /// <param name="bunnyCdnStorage"></param>
+        public BunnyCdnWebsiteResourceWriter(ILogger<BunnyCdnWebsiteResourceWriter> logger, BunnyCdnWebsiteResourceWriterConfiguration configuration, BunnyCDNStorage bunnyCdnStorage)
+        {
+            _logger = logger;
+            _bunnyCdnStorage = bunnyCdnStorage;
+            _configuration = configuration;
+        }
+
+        /// <inheritdoc/>
+        public async Task WriteResource(WebsiteResource websiteResource, CancellationToken token)
+        {
+            var key = _configuration.GenerateKey(websiteResource.RelativeUri);
+            var path = $"/{_configuration.StorageZoneName}/{key}";
+
+            using var stream = await websiteResource.ReadAsStream();
+
+            // Note: BunnyCDNStorage.UploadAsync does not currently take a CancellationToken.
+            await _bunnyCdnStorage.UploadAsync(stream, path);
+        }
+
+        /// <inheritdoc/>
+        public Task PrepareResources() => Task.CompletedTask;
+
+        /// <inheritdoc/>
+        public async Task FinishResources(IReadOnlyCollection<string> resources, CancellationToken token)
+        {
+            var allWrittenKeys = resources.Select(_configuration.GenerateKey).ToArray();
+
+            if (_configuration.ShouldCleanUnmatchedObjects)
+            {
+                var objects = await _bunnyCdnStorage.GetStorageObjectsAsync($"/{_configuration.StorageZoneName}/");
+                
+                var actualKeys = objects.Where(x => !x.IsDirectory).Select(x => x.ObjectName);
+
+                foreach (var unmatchedKey in actualKeys.Except(allWrittenKeys))
+                {
+                    var unmatchedPath = $"/{_configuration.StorageZoneName}/{unmatchedKey}";
+                    _logger.LogInformation("Removing unmatched key {UnmatchedKey} at path {UnmatchedPath}", unmatchedKey, unmatchedPath);
+                    await _bunnyCdnStorage.DeleteObjectAsync(unmatchedPath);
+                }
+            }
+        }
+    }
+}
---
+++ src/Ae.Freezer.BunnyCdn/BunnyCdnWebsiteResourceWriterConfiguration.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Ae.Freezer.BunnyCdn
+{
+    /// <summary>
+    /// Describes the configuration required for <see cref="BunnyCdnWebsiteResourceWriter"/>.
+    /// </summary>
+    public sealed class BunnyCdnWebsiteResourceWriterConfiguration
+    {
+        /// <summary>
+        /// The Bunny CDN Storage Zone name to use (must be set).
+        /// </summary>
+        public string StorageZoneName { get; set; }
+        /// <summary>
+        /// Whether to remove all objects from the storage zone which do not match the static website resources written in this session.
+        /// </summary>
+        public bool ShouldCleanUnmatchedObjects { get; set; }
+        /// <summary>
+        /// The function to use to generate the Bunny CDN object keys from the relative URIs.
+        /// By default, convert empty string (e.g. the root document) to "index.html" and use
+        /// the relative URL as the object key verbatim (<see cref="DefaultKeyGenerator"/>).
+        /// </summary>
+        public Func<string, string> GenerateKey { get; set; } = DefaultKeyGenerator;
+        /// <summary>
+        /// The default Bunny CDN key generator function.
+        /// </summary>
+        public static readonly Func<string, string> DefaultKeyGenerator = relativeUri =>
+        {
+            return relativeUri == string.Empty ? "index.html" : relativeUri;
+        };
+    }
+}
